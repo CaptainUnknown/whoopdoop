fb37d9aca1fa5a3b0908b8092ac61a6781c12fd0
CaptainUnknown <captainunknown7@gmail.com>
2022-05-31 16:13:45 +0500

    Added Moralis Provider

---
+++ merch/merch.js
@@ -0,0 +1,4 @@
+//Service Provider
+const serverUrl = "https://rzembgzcfjkd.usemoralis.com:2053/server";
+const appId = "WeJBsnqY0axj8C5iz05q1QT1qY0p9oX0XvG1VhRZ";
+Moralis.start({ serverUrl, appId });
