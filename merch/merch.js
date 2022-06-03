5bf9d6c2eca73e7ec7a40140252d80e2151dc745
CaptainUnknown <captainunknown7@gmail.com>
2022-06-03 14:57:11 +0500

    Fixed last image not registering

--- merch/merch.js
+++ merch/merch.js
@@ -14,6 +14,8 @@ const checkUser = () => {
     }
 }
 
+checkUser();
+
 //Authentication
 const login = async () => {
     let user = Moralis.User.current();
@@ -30,7 +32,7 @@ const login = async () => {
             })
             .catch(function(error) {
                 console.log(error);
-                alert(error.message);
+                alert(error.message);``
             });
     }
     setTimeout(function() {
@@ -44,8 +46,10 @@ const login = async () => {
 const logOut = async () => {
     await Moralis.User.logOut();
     console.log("logged out");
-    document.getElementById("btn-logout").style.visibility = "hidden";
-    document.getElementById("btn-login").style.visibility = "visible";
+    setTimeout(function(){
+        document.getElementById("btn-logout").style.visibility = "hidden";
+        document.getElementById("btn-login").style.visibility = "visible";
+   }, 400);
 }
   
   
@@ -80,37 +84,97 @@ document.getElementById("btn-login").addEventListener('click', () => {
 
 var ethNFTsImagesIPFS = []; //array of IPFS urls
 var ethNFTsContentIDs = []; //array of content IDs
-var ethNFTsImagesURLs = []; //array of NFT images 
+var ethNFTsImagesURLs = []; //array of NFT images
 
-var allImages = {
-    "eth" : ethNFTsImagesURLs,
-    //add all chains
-};
 
-//gets all the NFTs of the current user
+// ===================TEMPORARY============
+const ethNFTs = [
+    {
+    "name": "Wenolin",
+    "description": "test1",
+    "image": "ipfs://QmVZqAEa8BUQd8qmTfXgZfzRdptzNFMEGxFr2Aifixe56V/1.png",
+    "animation_url": "ipfs://QmcoAe8mixqscd6PTommSZpFF1oh46wgu1hX4x1gy5dYKG/1.glb",
+    "attributes": [
+      {
+        "trait_type": "House",
+        "value": "Alucar"
+      },
+
+      {
+        "trait_type": "Type",
+        "value": "Sword"
+      },
+
+      {
+        "trait_type": "Rarity",
+        "value": "Standard"
+      },
+
+      {
+        "trait_type": "Edition",
+        "value": "First Edition"
+      }
+    ]
+    },
+    {
+        "name": "Wenolin",
+        "description": "test1",
+        "image": "ipfs://QmVZqAEa8BUQd8qmTfXgZfzRdptzNFMEGxFr2Aifixe56V/1.png",
+        "animation_url": "ipfs://QmcoAe8mixqscd6PTommSZpFF1oh46wgu1hX4x1gy5dYKG/1.glb",
+        "attributes": [
+          {
+            "trait_type": "House",
+            "value": "Alucar"
+          },
+    
+          {
+            "trait_type": "Type",
+            "value": "Sword"
+          },
+    
+          {
+            "trait_type": "Rarity",
+            "value": "Standard"
+          },
+    
+          {
+            "trait_type": "Edition",
+            "value": "First Edition"
+          }
+        ]
+        }
+]
+console.log(ethNFTs);
+//=========================================
+
+
 const getNFTs = async() => {
     const packet = { chain: 'eth', address: currentUserAddress };
-    const ethNFTs = await Moralis.Web3API.account.getNFTs(packet);
+    //const ethNFTs = await Moralis.Web3API.account.getNFTs(packet);
     
-    for(let i = 0; i < ethNFTs.length - 1; i++) {
+    for(let i = 0; i < ethNFTs.length; i++) {
         let tempNFTObj = ethNFTs[i];
         let traits = Object.getOwnPropertyNames(tempNFTObj);
-        for(let j = 0; j < traits.length - 1; j++){
+        for(let j = 0; j < traits.length; j++){
             if(traits[j] == "image"){
                 ethNFTsImagesIPFS.push(tempNFTObj.image);
             }
         }
+        console.log("tempNFTObj", tempNFTObj);
     }
-    console.log(ethNFTsImagesIPFS);
-    for(let i = 0; i<ethNFTsImagesIPFS.length - 1 ; i++){
+    console.log("ethNFTsImagesIPFS", ethNFTsImagesIPFS);
+    for(let i = 0; i < ethNFTsImagesIPFS.length; i++){
+        console.log("Loop entered", ethNFTsImagesIPFS[i]);
         let temp = ethNFTsImagesIPFS[i];
         let tempIDs = temp.substring(7);
         ethNFTsContentIDs.push(tempIDs);
-        console.log(ethNFTsImages);
+        console.log(ethNFTsImagesURLs);
 
         ethNFTsImagesURLs.push(`https://ipfs.io/ipfs/${ethNFTsContentIDs[i]}`);
 
         console.log(ethNFTsImagesURLs[i]);
+
+        generateTable();
 
         /*
         let tempImage = ethNFTsImagesIPFS[i];
@@ -120,11 +184,41 @@ const getNFTs = async() => {
         document.getElementById("nft-container").appendChild(image);
         */
     }
-    return 0;
 }
 
+var selectedImages = [];
+
+const generateTable = () => {
+    for(let i = 0; i < ethNFTsImagesURLs.length; i++){
+        let imageContainer = document.getElementById("image-container");
+        let image = imageContainer.appendChild(document.createElement("li"));
+        image.innerHTML = `<li><input type=\"checkbox\" id=\"cb1\" /><label for=\"cb1\"><img src=\"${ethNFTsImagesURLs[i]}\" /></label></li>`;
+        element.addEventListener('click', function(){
+            selectedImages.push();
+            console.log("click");
+        });
+    }
+}
+
+/*
+<ul>
+  <li><input type="checkbox" id="cb1" />
+    <label for="cb1"><img src="https://picsum.photos/seed/1/100" /></label>
+  </li>
+  <li><input type="checkbox" id="cb2" />
+    <label for="cb2"><img src="https://picsum.photos/seed/2/100" /></label>
+  </li>
+  <li><input type="checkbox" id="cb3" />
+    <label for="cb3"><img src="https://picsum.photos/seed/3/100" /></label>
+  </li>
+  <li><input type="checkbox" id="cb4" />
+    <label for="cb4"><img src="https://picsum.photos/seed/4/100" /></label>
+  </li>
+</ul>
+*/
 
 //============================Image Generation================================
+/*
 const freeImgContainer = document.querySelector('.free-img-container');
 const freeImg = document.querySelector('#free-img');
 
@@ -171,7 +265,7 @@ const takeshot = () => {
                 link.download = "mycanvas.png";
             }, false);
         });
-}
+}*/
 
 /*
 const imgRo = new ResizeObserver((entries) => {
@@ -188,9 +282,11 @@ const imgRo = new ResizeObserver((entries) => {
 });
 imgRo.observe(freeImgContainer);
 */
+/*
 var canvasSrc = 'canvas.jpg';
 var freeImgSrc = 'free.png';
 
 document.getElementById('canvas-img').src = canvasSrc;
 document.getElementById('free-img').src = freeImgSrc;
+*/
 //==========================================================================
