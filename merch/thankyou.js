300547081334da79498fef8b1678b3fbbc0029e3
CaptainUnknown <captainunknown7@gmail.com>
2022-06-04 19:10:13 +0500

    Added Redirect functionality on Thank you page

---
+++ merch/thankyou.js
@@ -0,0 +1,12 @@
+if (document.cookie.userAddress == undefined) {
+    //window.location.replace("/merch/merch.html");
+}
+
+document.getElementById("shopMore").addEventListener("click", function() {
+    //redirect to merch.html
+    window.location.replace("merch.html");
+});
+
+document.getElementById("shopMore").addEventListener("click", function() {
+    window.location.replace("www.twitter.com"); //ADD PROFILE LINK
+});
